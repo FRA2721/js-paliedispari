a566fed2c1adf78a8e2b0ca5d7fffb95221e9176
FRA2721 <cimino.francesco36@gmail.com>
2022-10-14 17:08:51 +0200

    palindroma project done

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,5 +2,35 @@
 // project: palindroma;
 // language: javascript;
 
-// debug
-alert("red");
+const wordInput = prompt("Plese enter a generic word:");
+
+isPalindroma(wordInput);
+
+// function
+function isPalindroma(word) {
+
+    // word-palindroma-variable
+    let wordPalindroma= "";
+
+    // loop
+    for (let i = (word.length -1); i >= 0 ; i--) {
+        const index = word[i];
+        wordPalindroma += index;
+        console.log(wordPalindroma);
+    };
+
+    // palindroma-variable
+    let palindroma = false; 
+
+    // conditional-statements
+    if (wordPalindroma=== word) {
+        palindroma = true;
+        alert("The word that you wrote is palindroma... bye!");
+
+    } else {
+        console.log(palindroma);
+        alert("The word that you wrote is not palindroma... bye!");
+    };
+
+    return palindroma;
+}
